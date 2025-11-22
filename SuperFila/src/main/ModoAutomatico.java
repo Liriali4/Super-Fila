8b035a54e2a8c72396eb1da1ebc4307c0e12c61b
Liriali4 <lirialia51@gmail.com>
2025-11-22 12:19:14 +0100

    Feat: Inicialização do modo automático

---
+++ SuperFila/src/main/ModoAutomatico.java
@@ -0,0 +1,29 @@
+package main;
+
+import java.util.Scanner;
+
+public class ModoAutomatico {
+
+    public static void automatico() {
+        Scanner input = new Scanner(System.in);
+        int op;
+        System.out.println("\n\nModo automático activado!!!\n\n");
+
+        System.out.println("Qual é o tempo de atendimento de um produto?");
+        int tempoDeProduto = input.nextInt();
+
+        System.out.println("Qual é o número de caixas?");
+        int totalDeCaixas = input.nextInt();
+
+        System.out.println("Qual é o intervalo de tempo máximo entre clientes?");
+        int tempoEntreClientes = input.nextInt();
+
+        System.out.println("1. Monstrar fila de caixas.");
+        System.out.println("2. Criar clientes.");
+        System.out.println("3. Adicionar caixa.");
+        System.out.println("4. Retirar Caixa de atendimento.");
+        System.out.println("5. Atender T tempo.");
+        System.out.println("0. Encerrar modo automático.");
+
+    }
+}
